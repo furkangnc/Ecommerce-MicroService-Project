b0a1e163acaadf7f55d8fe6b323aafb4d7e7ea60
Furkan GENÇ <furkangenc61@gmail.com>
2023-08-16 13:23:41 +0300

    removed junk files

--- BtkAkademi.Services.Chat/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace BtkAkademi.Services.Chat.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
